689325d253b1855c266477bdca68dd0cf6710cbe
Marc Barbeau <mabarbeau1@gmail.com>
2019-04-21 18:45:01 -0700

    Create base card components.

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,99 @@
+<template>
+  <article class="card">
+    <div class="card-image-wrap">
+      <img src="https://lorempixel.com/500/200/" alt="" class="card-image">
+    </div>
+    <div class="card-info">
+      <div class="card-row">
+        <h3 class="card-right subtitle">$1,000,000</h3>
+        <h2 class="h5 card-left">
+          <span class="ellipsis">
+            {{ title }}
+          </span>
+        </h2>
+      </div>
+      <div class="card-row">
+        <slot></slot>
+      </div>
+      <div class="card-footer text-right">
+        <button type="button" name="button" class="btn btn--dense">
+          <span class="btn__label">
+            Foo
+          </span>
+        </button>
+        <button type="button" name="button" class="btn btn--dense btn--raised">
+          <i class="btn__icon material-icons">
+            add
+          </i>
+          <span class="btn__label">
+            Bar
+          </span>
+        </button>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard',
+  props: ['title', 'body'],
+};
+</script>
+
+<style lang="scss">
+.card{
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  position: relative;
+  margin: auto;
+  p{
+    margin: 0;
+  }
+}
+.card-image{
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-height: none;
+  max-width: none;
+  min-height: 100%;
+  min-width: 100%;
+  position: absolute;
+  /* background-color:  */
+}
+.card-image-wrap{
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  padding-top: 75%;
+}
+.card-info{
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  padding: 10px;
+  /* border: 1px solid $border-color; */
+}
+.card-footer{
+  margin-top: auto;
+}
+.card-row{
+  margin-bottom: 10px;
+}
+.ellipsis{
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  max-width: 100%;
+  overflow: hidden;
+  display: block;
+}
+.card-right{
+  float: right;
+}
+.card-left,
+.card-right{
+  margin: 0;
+}
+</style>
---
+++ src/components/BaseCardList.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="grid">
+        <div class="grid__inner">
+            <div v-for="post in posts" :key="post.id" class="grid__cell">
+                <BaseCard :title="post.title">
+                  {{ post.body }}
+                </BaseCard>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import BaseCard from './BaseCard.vue';
+
+export default {
+  name: 'BaseCardList',
+  components: { BaseCard },
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'My journey with Vue',
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 2,
+          title: 'Blogging with Vue',
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        {
+          id: 3,
+          title: 'Why Vue is so fun',
+          body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+      ],
+    };
+  },
+};
+
+</script>
