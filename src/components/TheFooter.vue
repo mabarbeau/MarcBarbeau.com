0c3483d7437d45313e14272b9ca287e5a061be2d
Marc Barbeau <mabarbeau1@gmail.com>
2019-04-21 18:44:21 -0700

    Create the footer.

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,39 @@
+<template>
+    <footer>
+      <a href="#">Source code</a>
+      <p>
+        Copyright (c) 2019 Copyright Holder All Rights Reserved.
+      </p>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'TheFooter',
+};
+</script>
+
+<style lang="scss" scoped>
+header{
+  position: fixed;
+  display: block;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+  background-color: white;
+}
+h1{
+  display: inline-block;
+}
+
+ul{
+  list-style: none;
+  text-align: center;
+  margin-top: 10px;
+  padding: 0;
+  li{
+    display: inline-block;
+  }
+}
+</style>
