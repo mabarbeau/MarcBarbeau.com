937ea6d496bd0f6302e72b9e8a808ae7601f0539
Marc Barbeau <mabarbeau1@gmail.com>
2019-05-03 09:09:19 -0700

    Fix lint errors.

--- src/components/BaseCardList.vue
+++ src/components/BaseCardList.vue
@@ -1,32 +1,32 @@
 <template>
-	<div class="grid">
-		<div v-if="posts" class="grid__inner">
-			<div v-for="post in posts" :key="post.id" class="grid__cell">
-				<BaseCard :post="post">
-					{{ post.body }}
-				</BaseCard>
-			</div>
-		</div>
-	</div>
+  <div class="grid">
+    <div v-if="posts" class="grid__inner">
+      <div v-for="post in posts" :key="post.id" class="grid__cell">
+        <BaseCard :post="post">
+          {{ post.body }}
+        </BaseCard>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import BaseCard from './BaseCard.vue';
 import axios from 'axios';
+import BaseCard from './BaseCard.vue';
 
 export default {
-	name: 'BaseCardList',
-	components: { BaseCard },
-	data() {
-		return {
-			posts: [],
-		};
-	},
-	created() {
-		axios
-			.get(`${this.$router.options.base}cards.json`)
-			// eslint-disable-next-line
-			.then(response => (this.posts = response.data));
-		}
-	};
+  name: 'BaseCardList',
+  components: { BaseCard },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  created() {
+    axios
+      .get(`${this.$router.options.base}cards.json`)
+    // eslint-disable-next-line
+      .then(response => (this.posts = response.data));
+  },
+};
 </script>
