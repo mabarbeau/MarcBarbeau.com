40b6ddca2529cb7014f449c334b97e9f742cb2af
Marc Barbeau <mabarbeau1@gmail.com>
2019-05-06 09:34:21 -0700

    Configure global sass imports.

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,22 @@
+const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/marcbarbeau.com/'
     : '/',
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'sass-loader',
-              options: {
-                includePaths: ['node_modules'],
-              },
-            },
-          ],
-        },
-      ],
-    },
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
   },
 };
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('sass-loader')
+    .options({
+      includePaths: ['node_modules'],
+      data: `
+          @import "@/styles /imports.scss";
+        `
+    })
+}
