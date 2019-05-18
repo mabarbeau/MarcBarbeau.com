138babd2770ea87ea5cd616cdff364e7fc268142
Marc Barbeau <mabarbeau1@gmail.com>
2019-05-18 11:30:42 -0700

    Fix lint error.

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,20 @@
-const path = require('path')
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/marcbarbeau.com/'
-    : '/',
-  chainWebpack: config => {
-    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
-    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)))
-  },
-};
-
-function addStyleResource (rule) {
+function addStyleResource(rule) {
   rule.use('style-resource')
     .loader('sass-loader')
     .options({
       includePaths: ['node_modules'],
       data: `
           @import "@/styles/imports.scss";
-        `
-    })
+        `,
+    });
 }
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/marcbarbeau.com/'
+    : '/',
+  chainWebpack: (config) => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type => addStyleResource(config.module.rule('scss').oneOf(type)));
+  },
+};
