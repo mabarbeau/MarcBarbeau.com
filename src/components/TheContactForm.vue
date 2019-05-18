acc37df7625e2332a538944ce915e35879efcc40
Marc Barbeau <mabarbeau1@gmail.com>
2019-05-18 11:31:29 -0700

    Contect the contact form with firebase.

--- src/components/TheContactForm.vue
+++ src/components/TheContactForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray">
-    <form class="grid bg-white" action="#" method="post">
+    <form @submit.prevent="onSubmit" id="contact" class="grid bg-white" action="#" method="post">
       <h2>Let's Start a Conversation.</h2>
       <div class="grid__inner">
         <div class="grid__cell--span-8">
@@ -11,6 +11,7 @@
             class="form-control"
             name="name"
             type="text"
+            v-model="name"
           >
         </div>
         <div class="grid__cell--span-4">
@@ -19,13 +20,20 @@
             id="email"
             placeholder="Email"
             class="form-control"
-            name="lastname"
+            name="email"
             type="text"
+            v-model="email"
           >
         </div>
         <div class="grid__cell--span-12">
           <label class="shown" for="firstname">Textarea</label>
-          <textarea class="form-control" placeholder="Textarea" name="message" rows="4"></textarea>
+          <textarea
+            class="form-control"
+            placeholder="Textarea"
+            name="message"
+            rows="4"
+            v-model="message"
+          ></textarea>
         </div>
         <div class="grid__cell--span-12 grid--align-right">
           <button class="btn btn--unelevated" type="submit" name="button">
@@ -40,10 +48,34 @@
 </template>
 
 <script>
+
+function writeMessage(name, email, message) {
+  window.db.collection("messages").add({
+    name,
+    email,
+    message
+  })
+  .then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+    console.error("Error adding document: ", error);
+  });
+}
+
 export default {
   name: 'TheContactForm',
+  data() {
+    return { name: null, email: null, message: null };
+  },
+  methods: {
+    onSubmit() {
+      writeMessage(this.name, this.email, this.message);
+    },
+  },
 };
 </script>
+
 <style scoped>
   .grid{
     max-width: 872px;
