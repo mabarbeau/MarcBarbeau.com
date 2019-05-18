2986eac0af3212589140d8599114cc5eff811ffd
Marc Barbeau <mabarbeau1@gmail.com>
2019-05-18 11:29:06 -0700

    Add firebase.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './styles/main.scss';
+import * as firebase from 'firebase/app';
+
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBJAIhG3ofLm-qi_stNEIgAciIMjJ49Go0',
+  authDomain: 'marcbarbeau.firebaseapp.com',
+  databaseURL: 'https://marcbarbeau.firebaseio.com',
+  projectId: 'marcbarbeau',
+  storageBucket: 'marcbarbeau.appspot.com',
+  messagingSenderId: '799811471396',
+  appId: '1:799811471396:web:ade0822ca40564fe',
+};
+firebase.initializeApp(firebaseConfig);
+
+window.db = firebase.firestore();
 
 Vue.config.productionTip = false;
 
